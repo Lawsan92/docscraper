395e94fd02e02a63e73970df6221254c0262bd11
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-04-24 16:58:31 -0500

    updated scritps and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path');
-const config = {
-  mode: 'development',
-  entry: {
-    path: path.join(__dirname, './client/src/index.js')
-  },
-  output: {
-    path: path.join(__dirname, './client/dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /nodeModules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(css|scss)\b/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-    ]
+const config = (env) => {
+  console.log('env:', env);
+  return {
+    mode: env['--mode'],
+    entry: {
+      path: path.join(__dirname, './client/src/index.js')
+    },
+    output: {
+      path: path.join(__dirname, './client/dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          exclude: /nodeModules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|scss)\b/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+      ]
+    }
   }
 }
 
 module.exports = config;
+
